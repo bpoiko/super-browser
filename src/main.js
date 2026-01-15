5a3239801264733d13c85d817fa2f5766be60f5a
Braylon <epicb35@gmail.com>
2026-01-15 02:53:10 -0800

    updating comments

--- src/main.js
+++ src/main.js
@@ -57,4 +57,4 @@ app.on('window-all-closed', () => {
 });
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+// code. You can also put them in separate files and import them here. make sure imports render though... unused imports = possible memory leaks
